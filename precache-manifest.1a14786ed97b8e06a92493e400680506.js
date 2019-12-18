38b870f63f0d24591ddd9b5937566fee7889c208
“kim” <kimbfsd@gmail.com>
2019-12-18 03:04:17 +0200

    Updates

---
+++ precache-manifest.1a14786ed97b8e06a92493e400680506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1315f3ea3548eb9794337ded9b9c36c9",
+    "url": "/post-app/index.html"
+  },
+  {
+    "revision": "35aae5c1070686def2c5",
+    "url": "/post-app/static/css/main.568cf339.chunk.css"
+  },
+  {
+    "revision": "fad6b72955d2e6c54e1a",
+    "url": "/post-app/static/js/2.4b0763c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/post-app/static/js/2.4b0763c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35aae5c1070686def2c5",
+    "url": "/post-app/static/js/main.31550f39.chunk.js"
+  },
+  {
+    "revision": "21cc585dbb8ac0cf03a3",
+    "url": "/post-app/static/js/runtime-main.fdc9ad48.js"
+  }
+]);
